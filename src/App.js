0654abfbecf0eb146ec95f137299366c0177da06
Caroline Madison <carolinemariemadison@gmail.com>
2023-10-03 21:09:59 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import './App.css';
 import { ApplicationViews } from './ApplicationViews';
 import { NavBar } from './NavBar';
 
-
-
-
 export const DecorationStation = () => {
-
- 
 
   return (
     <>
       <NavBar />
       <ApplicationViews />
     </>
-
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,18 +3,10 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
     return (
         <>
-        <Link to="new">
-            Add New Decoration
-            </Link>
-        <Link to="halloween">
-            Halloween
-        </Link>
-        <Link to="christmas">
-            Christmas
-        </Link>
-        <Link to="thanksgiving">
-            Thanksgiving
-        </Link>
+            <Link to="new">Add New Decoration</Link>
+            <Link to="halloween">Halloween</Link>
+            <Link to="christmas">Christmas</Link>
+            <Link to="thanksgiving">Thanksgiving</Link>
         </>
     )
 }
